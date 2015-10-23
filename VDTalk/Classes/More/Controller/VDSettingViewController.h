189fe85582eee2a42fdc5f09f9aef13b850bdb1a
Haijun <whj929159021@hotmail.com>
2015-10-24 01:52:17 +0800

    日常更新

---
+++ VDTalk/Classes/More/Controller/VDSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  VDSettingViewController.h
+//  VDTalk
+//
+//  Created by Haijun on 10/24/15.
+//  Copyright © 2015 Harwyn T'an. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VDSettingViewController : UITableViewController
+
+@end
